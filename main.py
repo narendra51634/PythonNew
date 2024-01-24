eb48747960889f7d8bd6ce9f997eadae90f72e4e
Narendra <todatnarendra@gmail.com>
2024-01-24 04:13:56 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+no1 = input("Enter first No");
+no2  =input("Enter Second Number");
+operater  =input("Enter Operator");
+print("Adtion Of Two No is ", int(no1)+int(no2)); 
+
+a = input("Enter first No");
+b  =input("Enter Second Number");
+operater  =input("Enter Operator");
+print("Adtion Of Two No is ", int(a)-int(b)); 
+
+
+
+
